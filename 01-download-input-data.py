ab5821431489cbcacd07502f115d5f38b263ca94
Eric Lawrey <e.lawrey@aims.gov.au>
2025-06-16 20:07:36 +1000

    Buffer clip benchmark

--- 01-download-input-data.py
+++ 01-download-input-data.py
@@ -15,6 +15,17 @@ direct_download_url = 'https://nextcloud.eatlas.org.au/s/8yzFHQkLxXBLM2G/downloa
 downloader.download_and_unzip(direct_download_url, 'habitat', flatten_directory=True)
 
 
+# --------------------------------------------------------
+# Australian Coastline 50K 2024 (NESP MaC 3.17, AIMS)
+# https://eatlas.org.au/geonetwork/srv/eng/catalog.search#/metadata/c5438e91-20bf-4253-a006-9e9600981c5f
+# Hammerton, M., & Lawrey, E. (2024). Australian Coastline 50K 2024 (NESP MaC 3.17, AIMS) (2nd Ed.) [Data set]. eAtlas. https://doi.org/10.26274/qfy8-hj59
+#direct_download_url = 'https://nextcloud.eatlas.org.au/s/DcGmpS3F5KZjgAG/download?path=%2FV1-1%2F&files=Split'
+#downloader.download_and_unzip(direct_download_url, 'AU_AIMS_Coastline_50k_2024', subfolder_name='Split', flatten_directory=True)
+
+# Use this version for overview maps
+direct_download_url = 'https://nextcloud.eatlas.org.au/s/DcGmpS3F5KZjgAG/download?path=%2FV1-1%2F&files=Simp'
+downloader.download_and_unzip(direct_download_url, 'AU_AIMS_Coastline_50k_2024', subfolder_name='Simp', flatten_directory=True)
+
 # These are for later development of the shallow mapping test
 
 # --------------------------------------------------------
---
+++ 03-bench-buffer-clip.py
@@ -0,0 +1,33 @@
+import time
+import geopandas as gpd
+from shapely.geometry import Polygon, MultiPolygon
+
+start_time = time.time()
+
+print("Loading coastline shapefile...")
+coast_fp = "data/AU_AIMS_Coastline_50k_2024/Simp/AU_NESP-MaC-3-17_AIMS_Aus-Coastline-50k_2024_V1-1_simp.shp"
+gdf = gpd.read_file(coast_fp)
+
+print("Unioning all features...")
+coast_union = gdf.geometry.union_all()
+
+print("Buffering by 0.05 degrees (~5km)...")
+buffered = coast_union.buffer(0.05)
+
+print("Clipping off land (difference)...")
+near_coast = buffered.difference(coast_union)
+
+print("Simplifying geometry...")
+near_coast_simple = near_coast.simplify(0.001)
+
+print("Preparing output GeoDataFrame...")
+if isinstance(near_coast_simple, (Polygon, MultiPolygon)):
+    out_gdf = gpd.GeoDataFrame(geometry=[near_coast_simple], crs=gdf.crs)
+else:
+    out_gdf = gpd.GeoDataFrame(geometry=list(near_coast_simple), crs=gdf.crs)
+
+print("Saving to shapefile...")
+out_fp = "output/03/near-coast-mask.shp"
+out_gdf.to_file(out_fp)
+
+print(f"Script completed in {time.time() - start_time:.2f} seconds.")
